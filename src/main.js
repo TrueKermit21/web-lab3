89222a76392bd92e0de870180de8c6eed8998815
TrueKermit21 <igromaniaasd@gmail.com>
2025-05-29 16:32:04 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/style.css'
+import './web-lab3/src/assets/css/style.css'
 
 const app = createApp(App)
 app.use(router)
